f31abd3d6809c04b0c0f23ca0c027598e9089509
郭宪强 <guoxq@wanhuhealth.com>
2021-06-23 19:12:05 +0800

    修改

--- assets/feBooks/bookData.js
+++ assets/feBooks/bookData.js
@@ -1,16 +1,16 @@
 let bookData=[
   {
-    cover:'/feBooks/',
-    name:'',
+    cover:'/feBooks/html5gjcxsj-min.jpg',
+    name:'HTML5高级程序设计',
     author: '',
-    authorDes: '',
-    des:'',
-    downloadUrl:'',
-    downloadCode:''
+    authorDes: 'Peter Lubbers Kaazing文档培训主管，旧金山HTML5用户组创建人。作为HTML5和WebSocket的狂热爱好者，Peter常常在国际大会上发言，还在全球范围内开展HTML5的技术培训。在加盟Kaazing前，Peter在Oracle担任了近十年的资深信息架构师，获得过两项软件专利。|||Brian Albers Kaazing研发中心副总裁。他有数十年的Web开发经验，曾在Oracle担任高级开发经理。Brian是一位讲演常客，经常在Web 2.0博览会、AJAXWorld博览会和Web JavaOne等国际性会议上做讲演。|||Frank Salim Kaazing的元老级工程师，曾参与过WebSocket网关和客户端策略项目。他毕业于波莫纳学院计算机专业，除了编程之外，还喜欢阅读、绘画和轮滑。',
+    des:'今天，大多数现代浏览器都已经支持HTML5。学习体验HTML5带给Web开发的便捷、快速和强大功能，是每一位Web设计和开发人员的当务之急。|||本书由旧金山HTML5用户组创建人联合另两位资深Web开发专家共同打造，为读者清晰解读了HTML5规范的缘由、发展和现状，全面展示了如何使用WebSocket、Geolocation、Web Storage、Canvas及音频视频等前所未有的新特性构建先进的Web应用，并以大量的示例涵盖全部HTML5 API。此外，还介绍了当今浏览器对HTML5特性的支持情况。',
+    downloadUrl:'https://pan.baidu.com/s/1Ks7umBsaXD19-VF0ZP9UKA',
+    downloadCode:'078n'
   },
   {
     cover:'/feBooks/',
-    name:'',
+    name:'数据结构与算法JavaScript描述',
     author: '',
     authorDes: '',
     des:'',
