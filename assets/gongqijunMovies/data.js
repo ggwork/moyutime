2a4b363c4358f89d54c23873c23b42216d59096e
郭宪强 <guoxq@wanhuhealth.com>
2021-07-22 19:04:03 +0800

    宫崎骏

---
+++ assets/gongqijunMovies/data.js
@@ -0,0 +1,176 @@
+let bookData=[
+  {
+    cover:'/gongqijunMovies/lbss.webp',
+    name:'鲁邦三世：卡里奥斯特罗城',
+    author: '宫崎骏',
+    authorDes: '',
+    des:'卡里奥斯特罗，一个位于欧洲人口只有3500人的小国家，却是世界上最逼真的伪钞——山羊之钞的发源地。其伪钞之真，连鲁邦三世这样的大盗都险些被其蒙蔽。鲁邦和伙伴旅行至此，恰巧遇见正被一伙暴徒追赶的美丽少女克拉丽斯。鲁邦出手相救，却从少女遗留下的戒指上若有所悟。原来是多年前鲁邦曾来此寻找过山羊之钞的下落，并在此结识还是孩童的克拉丽斯及其身为大公的父亲。如今物是人非，七年前的一场大火烧毁了大公的城堡，克拉丽斯的父母皆在大火中身亡。孤苦伶仃的克拉丽斯无依无靠，而摄政王卡里奥斯特罗伯爵则逼迫少女与之成亲。鲁邦决定从伯爵手中救出女孩，同时探究戒指背后所隐藏的关于宝藏的秘密……',
+    outerDownloadUrl:'',
+    downloadUrl:'https://pan.xunlei.com/s/VMfCehC4KHtFGD_4Drx77WXZA1',
+    downloadCode:'62i5'
+  },
+  {
+    cover:'/gongqijunMovies/lm.webp',
+    name:'龙猫',
+    author: '宫崎骏',
+    authorDes: '',
+    des:'小月的母亲生病住院了，父亲带着她与四岁的妹妹小梅到乡间的居住。她们对那里的环境都感到十分新奇，也发现了很多有趣的事情。她们遇到了很多小精灵，她们来到属于她们的环境中，看到了她们世界中很多的奇怪事物，更与一只大大胖胖的龙猫成为了朋友。龙猫与小精灵们利用他们的神奇力量，为小月与妹妹带来了很多神奇的景观，令她们大开眼界。|||妹妹小梅常常挂念生病中的母亲，嚷着要姐姐带着她去看母亲，但小月拒绝了。小梅竟然自己前往，不料途中迷路了，小月只好寻找她的龙猫及小精灵朋友们帮助。',
+    outerDownloadUrl:'',
+    downloadUrl:'https://pan.xunlei.com/s/VMfCfA-7RCOlliexZ0KZrSzNA1',
+    downloadCode:'zakm'
+  },
+  {
+    cover:'/gongqijunMovies/mnzjb.webp',
+    name:'魔女宅急便',
+    author: '宫崎骏',
+    authorDes: '',
+    des:'琪琪今年13岁了，按照魔法界的规矩，魔法少女年满13岁就要出外进行为期一年的修行。所以琪琪带着宠物黑猫吉吉踏上了修行之旅。然而，修行之旅开始得并不顺利，当琪琪来到海边一座大城市时，人们并没有欢迎她的到来，人人都不搭理她。幸亏琪琪有一颗善良的心，当她拾到了一件别人的失物时，热心的琪琪找到了失主。她的善良赢得了面包店老板娘的好感，琪琪就在面包店开始了她用飞行魔法为客人服务的快递业务。琪琪很快适应了新环境，一次，一个热衷于飞机制造的男孩邀请琪琪去参加飞行俱乐部的聚会，途中琪琪因为帮一位老大娘送东西而被雨淋了。从此，琪琪突然发现自己的魔法正在一天天变弱。',
+    outerDownloadUrl:'',
+    downloadUrl:'https://pan.xunlei.com/s/VMfCls1o0wXfF3jrCmmbBv7kA1',
+    downloadCode:'2ptx'
+  },
+  {
+    cover:'/gongqijunMovies/qfl.webp',
+    name:'起风了',
+    author: '宫崎骏',
+    authorDes: '',
+    des:'那是一个动荡的年代，战争、饥饿、疾病、天灾，死神的阴影笼罩全球，却无法遮挡心头闪现的梦想和希望的光芒。平凡的日本少年堀越二郎（庵野秀明 配音）迷恋游弋长空的飞机，梦中他与意大利飞机设计师卡普罗尼（野村万斋 配音）成为朋友，享受着飞行带来的乐趣。恐怖的关东大地震，二郎邂逅一生的挂念——里见菜穗子（泷本美织 配音），短暂的相逢，无序的混乱中又失之交臂。在命运的指引下，被称为天才的二郎如愿成为飞机设计师，他和前辈同侪共同努力，绞尽脑汁提升飞机的性能。欧洲的先进设计让他们痛心疾首，另一方面又充满矛盾的打造着融合了梦想和杀人属性的机器。他追逐梦想，并与菜穗子重逢。动荡的昭和时代，风雪飘摇之中步伐从未停止……',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/gongqijunMovies/fzg.webp',
+    name:'风之谷',
+    author: '宫崎骏',
+    authorDes: '',
+    des:'工业文明的高度发展，却最终带来灾难，一场为期7天的虫族毁灭战争摧毁了人类文明。一千年后，工业文明遗留下来的症结仍在困扰这个星球。带有强烈毒性的植物组成了令人望而却步的“腐海”森林，随着时间的推移，腐海有条不紊地向四周蔓延，蚕食周边世界。在一片腐海森林的旁边，居住着一群御风而行的人类。女主角娜乌茜卡和族人依靠海风的力量抵御着有毒孢子的侵蚀，过着平静祥和的生活。|||直到某一天，战火烧到了这里。西方强国特洛梅意欲重新建立人类霸权，他们计划统一周边诸国，然后向虫族发起总攻。另一方面，一千年前曾主导摧毁人类的上古怪兽“猛怪”重现人间。毁灭战争一触即发，人类的命运将会如何……',
+    outerDownloadUrl:'',
+    downloadUrl:'https://pan.xunlei.com/s/VMfCnzJwOxsPzdKwMycAwav9A1',
+    downloadCode:'pe6a'
+  },
+  {
+    cover:'/gongqijunMovies/hedydcb.webp',
+    name:'哈尔的移动城堡',
+    author: '宫崎骏',
+    authorDes: '',
+    des:'继母因无力负担生活，将苏菲和她的两个妹妹都送到了制帽店去当学徒。两个妹妹很快先后就离开了制帽店去追寻各自的梦想，只有苏菲坚持了下来。一天，小镇旁边来了一座移动堡垒，传说堡垒的主人哈尔专吸取年青姑娘的灵魂，所以小镇的姑娘都不敢靠近。|||一个恶毒的巫婆为了报复苏菲与哈尔见面，用巫术把她变成了一个80岁的老太婆，而且苏菲还不能对别人说出自己身中的巫术。无奈，苏菲决定独自一人逃离小镇。天黑了，虚弱的苏菲没走多远，来到了移动城堡。心想自己已经是老太婆了，苏菲壮着胆子走进了城堡。不想，遇到了和她遭遇相同的火焰魔。两人约定彼此帮助对方打破各自的咒语……',
+    outerDownloadUrl:'',
+    downloadUrl:'https://pan.xunlei.com/s/VMfCoLb6Ngp0oIwHFAKwoo8tA1',
+    downloadCode:'y22f'
+  },
+  {
+    cover:'/gongqijunMovies/mdbe.webp',
+    name:'猫的报恩',
+    author: '宫崎骏',
+    authorDes: '',
+    des:'吉冈春（池胁千鹤 配音）是一个普普通通同时又有些迷糊的女高中生，某天放学的路上，她偶然救下了一只险些丧生车轮之下的小猫，谁曾想小猫竟然直立起来以人类的方式对女孩表示感谢。小春更想不到的是，她不经意的善举改变了自己的命运。原来小猫是猫王国的王子，为了报答小春的恩情，猫王国举国上下展开了令人啼笑皆非的报恩行动，只不过它们视为“上品”的老鼠令小春哭笑不得。不久后，小春接受王国的邀请前去做客，期间竟稀里糊涂答应成为王子的妃子。女孩眼看就彻底变成了一只猫，神秘的男爵（袴田吉彦 配音）成为她的救命稻草……',
+    outerDownloadUrl:'',
+    downloadUrl:'https://pan.xunlei.com/s/VMfCoa-dKHtFGD_4Drx79aNHA1',
+    downloadCode:'4d76'
+  },
+  {
+    cover:'/gongqijunMovies/wdljstj.webp',
+    name:'我的邻居山田君',
+    author: '高畑勋',
+    authorDes: '',
+    des:'住在山野市的山田家共有五口人：一家之主山田隆年届不惑，是一间小公司杂物课的职员。他有些大男子主义，经常为了些琐事和老婆吵吵闹闹，却又被丈母娘压得抬不起头来；阿隆的妻子松子和丈夫同岁，是个全职的家庭主妇，每天忙忙碌碌，照顾一大家子人的饮食起居，却又迷迷糊糊，经常丢三落四；松子的母亲年近古稀，然而精神矍铄，这位老人家一向特立独行，讲话毒辣，连暴走组也全不畏惧；长子阿升是个长相和学习都平平的中学生，他继承了母亲迷糊的性格，生活中乐事不断；小女儿野野子还是个小学三年级学生，瘦瘦小小的她碰上一群脑瓜秀逗的家人，经常成为被遗忘的对象。',
+    outerDownloadUrl:'',
+    downloadUrl:'https://pan.xunlei.com/s/VMfCp3CgNgp0oIwHFAKwoy2IA1',
+    downloadCode:'pt6m'
+  },
+  {
+    cover:'/gongqijunMovies/hyjwy.webp',
+    name:'辉夜姬物语',
+    author: '高畑勋',
+    authorDes: '',
+    des:'住在深山老林里的老爷爷（地井武男 配音）一早进山砍竹子，结果竟在附近的一根发光的竹笋中发现了只有掌心大小的娃娃。没有子女的老爷爷兴奋不已，他双手捧着娃娃回到家中，而小家伙竟然在老奶奶（宫本信子 配音）的手中越变越大。之后的岁月里，舐犊情深的爷爷小心呵护他的公主（朝仓亚纪 配音），小女孩则以惊人的速度生长，她与村里的孩子们周游乡间，结识了诚实可靠的舍丸哥哥（高良健吾 饰），而一首莫名的歌曲却令她潸然泪下。老爷爷在山里先后发现金子和美丽布匹，他决心让公主过上贵族的生活，于是举家搬到了京城的豪宅中。公主超凡脱俗，貌美绝伦，从此“辉耀姬”的名字在京城贵族间广为传颂。然而美丽女孩的笑容渐渐退去，这似乎并不是她所向往的生活……',
+    outerDownloadUrl:'',
+    downloadUrl:'https://pan.xunlei.com/s/VMfCpL_uRCOlliexZ0KZtXniA1',
+    downloadCode:'tig9'
+  },
+  {
+    cover:'/gongqijunMovies/yhczm.webp',
+    name:'萤火虫之墓',
+    author: '高畑勋',
+    authorDes: '',
+    des:'美日战争爆发，14岁的清太带着年幼的妹妹到处逃命，当他们到达防空洞的时候，母亲已身受重伤，没过多久便不久人世。两兄妹自此过着相依为命的日子。他们只好投靠了母亲的姐妹，纵使他们把家里所有的家当都送给了阿姨，但当食物吃完，他们就越感到了寄人篱下的感觉，清太决定带着妹妹离开独自生活。火灾硝烟中的生活并不好过，清太为了妹妹四处偷取食物，但妹妹还是严重营养不良……',
+    outerDownloadUrl:'',
+    downloadUrl:'https://pan.xunlei.com/s/VMfCpdv3RCOlliexZ0KZtbO6A1',
+    downloadCode:'ak8i'
+  },
+  {
+    cover:'/gongqijunMovies/hz.webp',
+    name:'红猪',
+    author: '宫崎骏',
+    authorDes: '',
+    des:'罗森是一战中意大利空军的王牌飞行员，但却被施了魔法变了一头猪。虽然变了猪，但罗森还是继续他的老本行，成了一位赏金猎人，专门打击在亚地里亚海空域作恶的空中劫匪。红猪侠一时威名远扬，令劫匪们闻风丧胆。匪徒们为了对付罗森，请来了超级飞行员卡基斯。卡基斯在于罗森对决中趁他发动机故障将其击落。罗森把飞机送到老朋友比克罗处修理时，遇上了比克罗的孙女菲欧。菲欧帮罗森修好了飞机，两人一起再度翱翔蓝天对付空中劫匪。',
+    outerDownloadUrl:'',
+    downloadUrl:'https://pan.xunlei.com/s/VMfCptIKojCHkUAGZhW-mZ1hA1',
+    downloadCode:'hc5a'
+  },
+  {
+    cover:'/gongqijunMovies/sysn.webp',
+    name:'神隐少女',
+    author: '宫崎骏',
+    authorDes: '',
+    des:'《千与千寻》是宫崎骏执导、编剧，柊瑠美、入野自由、中村彰男、夏木真理等配音，吉卜力工作室制作的动画电影。该片讲述了少女千寻意外来到神灵异世界后，为了救爸爸妈妈，经历了很多磨难的故事。该片于2001年7月20日在日本上映，后于2019年6月21日在中国正式上映。',
+    outerDownloadUrl:'',
+    downloadUrl:'https://pan.xunlei.com/s/VMfCqg0LpRV1YgjXYNOZzGiRA1',
+    downloadCode:'dvb6'
+  },
+  {
+    cover:'/gongqijunMovies/',
+    name:'听见涛声',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/gongqijunMovies/',
+    name:'',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/gongqijunMovies/',
+    name:'',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/gongqijunMovies/',
+    name:'',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/gongqijunMovies/',
+    name:'',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+
+  
+]
+
+export default bookData
