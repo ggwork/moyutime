6a2501474d411bdf227744c16d1d19ecdb7f109d
郭宪强 <guoxq@wanhuhealth.com>
2021-07-23 13:39:18 +0800

    添加治愈系电影

---
+++ assets/janpanWarmMovies/data.js
@@ -0,0 +1,318 @@
+let bookData=[
+  {
+    cover:'/janpanWarmMovies/jcldxt.webp',
+    name:'菊次郎的夏天',
+    author: '北野武',
+    authorDes: '',
+    des:'暑假到来，自幼丧父的小学三年级学生正男（关口雄介饰），如今和奶奶生活在一起，暑假特别无聊。他拿出母亲从外地寄回的信，准备自己一个人去爱知县丰桥市看望母亲。|||邻居阿姨（岸本加世子饰）发现后，决定帮正男完成心愿。她拿出一笔钱做为旅费，还安排游手好闲的老公菊次郎（北野武饰）陪伴正男一起踏上寻母之途。可是在第一天，菊次郎就把老婆给的钱全部输光，于是两个人只好步行前往爱知。|||一路搭顺风车，惹了不少麻烦后，菊次郎终于带着正男到了他母亲的家，但小男孩却十分沮丧。归途中，菊次郎努力安慰他，二人过得十分愉快，夏天就这么过去了。',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/wyggdsgyd.webp',
+    name:'我与狗狗的十个约定',
+    author: '本木克英',
+    authorDes: '',
+    des:'十四岁的少女齐藤明莉（福田麻由子 饰）收养了一只可爱的小狗，在妈妈的建议下她给小狗取名为“索克斯”，并和小狗定下“十个约定”。不久，妈妈去世，而身为医生的爸爸忙于工作，无暇多照顾明莉，“索克斯”便成为明莉最忠实的玩伴。明莉在学校还有个梦想成为演奏家的朋友星进，当明莉因为爸爸工作调动搬家时，“索克斯”便寄养在星进家。可是星进（佐藤祥太 饰）要去国外留学时，爸爸却因为急诊没有按时送他，明莉没能见到星进最后一面。为了弥补对女儿长期以来的亏欠，爸爸决定辞职开起了小诊所，“一家三口”又重新生活在了一起。长大后的明莉开始了忙碌的生活，陪伴“索克斯”和爸爸的时间也越来越少。后来明莉（田中丽奈 饰）又遇见学成归来的星进（加濑亮 饰），两人还成为相爱的恋人。很快，距离初遇索克斯已过去十年，“索克斯”也走到了生命的尽头，这时，明莉又想起了当初与“索克斯”的十个约定……',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/qs.webp',
+    name:'情书',
+    author: '岩井俊二',
+    authorDes: '',
+    des:'日本神户某个飘雪的冬日，渡边博子（中山美穗）在前未婚夫藤井树的两周年祭日上又一次悲痛到不能自已。正因为无法抑制住对已逝恋人的思念，渡边博子在其中学同学录里发现“藤井树” 在小樽市读书时的地址时，依循着寄发了一封本以为是发往天国的情书。|||不想不久渡边博子竟然收到署名为“藤 井树（中山美穗）”的回信，经过进一步了解，她知晓此藤井树是一个同她年纪相仿的女孩，且还是男友藤井树（柏原崇）少年时代的同班同学。为了多了解一些昔日恋人在中学时代的情况，渡边博子开始与女性藤井树书信往来。而藤井树在不断的回忆中，渐渐发现少年时代与她同名同姓的那个藤井树曾对自己藏了一腔柔情。',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/xszq.webp',
+    name:'星守之犬',
+    author: '泷本智行',
+    authorDes: '',
+    des:'位于北海道的某个小城市，在市政府工作的奥津（玉山铁二 饰）正和同事前往密林深处，不久前此处发现一辆废弃的汽车，车内则有一具中年男子和一条狗的骸骨。汽车内外除了几张沿途消费小票外，没有任何能够证明该男子身份的证件。经对骸骨的检验得知，男人死后长达半年时间狗狗才随之死去。童 年有着养狗经历的奥津对男人的经历倍感好奇，他自费前往死者旅程的起点——东京调查，并在路上搭载了偷偷跑到东京选秀的同乡少女有希（川岛海荷 饰）。二人以购物小票为线索，一路追寻着中年男子“前田”（西田敏行 饰）及其爱犬Happy的足迹。经过探访，他们渐渐拼凑出“前田”先生那平凡寂寥的人生……',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/xfdhsp.webp',
+    name:'幸福的黄手帕',
+    author: '山田洋次',
+    authorDes: '',
+    des:'煤矿工人勇作（高仓健）失手将一名寻衅滋事的流氓打死后，被判刑入狱，在监狱里，他非常想念自己的妻子光枝（倍赏千惠子），却因害怕耽误她的生活和前途，主动提出离婚，并真心实意地劝她改嫁。|||刑满释放前，勇作给光枝写了一封信，说如果她还没有与他人结婚，仍在等待着他的归去，就在家门前的旗杆上挂上一块黄手帕，假如他看不到黄手帕，会就此远走他乡。归家途中，患得患失的勇作碰到一对驾车出行的年轻情侣（武田铁矢、桃井薰），搭乘他们的车游转了一些地方，之后，他才鼓足勇气回到家乡。 ',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/btt.webp',
+    name:'白兔糖',
+    author: '萨布',
+    authorDes: '',
+    des:'27岁的上班族河地大吉（松山健一 饰）赶回家乡参加外公的葬礼，意外见到了外公的私生女——美丽而可怜的鹿贺凛（芦田爱菜 饰）。凛自然无法得到鹿贺家族的认可，却引起了大吉的关注与同情。最终，大吉将这个年仅6岁、尚在尿床的小阿姨带回东京独自抚养。凛的到来彻底改变了单身汉大吉的生活，他半天在公司辛苦奔忙，回到家还要照顾凛的衣食起居，随着凛到了进入幼稚园的年龄，奔跑逐渐成为大吉生活的主题。在这一过程中，曾经紧紧封闭内心的凛逐渐向大吉敞开心扉，他们既是奇怪的父女，又是年龄倒错的阿姨和外甥，平淡的生活充满幸福……',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/hdszj.webp',
+    name:'横道世之介',
+    author: '冲田修一',
+    authorDes: '',
+    des:'在20世纪80年代末，出生在海港小镇的青年横道世之介（高良健吾 饰）一脸懵懂地出现在东京街头。正如他那个有些奇怪的名字一样，世之介仿佛与现代化大都会有着天然的差距，不过这个天生乐观且迂直的大男孩很快融入到新生活之中。在喧闹的校园里，他最先与仓持（池松壮亮 饰）、阿久津唯（朝仓亚纪 饰）相识，又和藏有秘密的加藤雄介（绫野刚 饰）短暂交往，与纯真无邪的富家千金与谢野祥子（吉高由里子 饰）谱写了一段浪漫恋情，也似乎曾对有着丰富人生阅历的千春姐（伊藤步 饰）心怀好感。此去经年，这些人偶尔停下脚步，摹地便想起那个曾给他们的生命留下美好回忆的世之介，心中会涌起怎样的情感？',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/qzy.webp',
+    name:'青之炎',
+    author: '蜷川幸雄',
+    authorDes: '',
+    des:'国中二年级学生栉森秀一（二宫和也 饰）与母亲友子（秋吉久美子 饰）、妹妹遥香（铃木杏 饰）住在一起。秀一的父亲早年间车祸去世，十年前母亲改嫁曾根隆司（山本寛斎 饰），而这段婚姻又因为曾根的暴力倾向而夭折。|||某天，曾根返回栉森家。秀一发现，母亲似乎有某种隐忧而不愿将曾根赶 出家门，另一方面，曾根好像又对遥香心怀不轨。对继父心存愤恨的秀一无计可施，于是通过网络购买碳酸钾，决定将曾根杀于无形。他的计划顺利实施，但他本人也越陷越深……',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/dbrdc.webp',
+    name:'第八日的蝉',
+    author: '成岛出',
+    authorDes: '',
+    des:'一段不伦之恋，造成了两代人的人生悲剧。野野宫希和子（永作博美 饰）无法得到情人的身与心，还被迫杀掉了腹中的孩子。在一个瓢泼大雨的日子里，万念俱灰的希和子走入情人的家，抱走了他们尚在襁褓中的女儿惠理菜。此后的岁月中，她带着“女儿”薰辗转各地，只为寻求“母女”安身立命的宁静之所。此去经年，长大成人的惠理菜（井上真央 饰）经历着人生的种种，而她似乎正在走一条与“母亲”希和子相同的路。无论多么不愿回首，她仍按照宿命的指引追忆着与希和子共处的那段短暂而美好的时光……',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'东京家族',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'黄昏的清兵卫',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'再见歌舞伎町',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'听说桐岛要退部',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'哪啊哪啊神去村',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'海街日记',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'小森林夏秋篇',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'山樱',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'导盲犬小Q',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'如父如子',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'借着雨点说爱你',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'南极物语',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'记我的母亲',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'厕所女神',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'再见我们的幼儿园',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'猫咪物语',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'小森林冬春篇',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'入殓师',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'步履不停',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'五个扑水少年',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  {
+    cover:'/janpanWarmMovies/',
+    name:'奇迹',
+    author: '',
+    authorDes: '',
+    des:'',
+    outerDownloadUrl:'',
+    downloadUrl:'',
+    downloadCode:''
+  },
+  // {
+  //   cover:'/janpanWarmMovies/',
+  //   name:'',
+  //   author: '',
+  //   authorDes: '',
+  //   des:'',
+  //   outerDownloadUrl:'',
+  //   downloadUrl:'',
+  //   downloadCode:''
+  // },
+
+
+
+  
+]
+
+export default bookData
