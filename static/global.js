357b5bcfd924448b1108027f57deaf90025ddead
郭宪强 <guoxq@wanhuhealth.com>
2022-06-02 13:45:27 +0800

    修改bug

--- static/global.js
+++ static/global.js
@@ -1,13 +1,7 @@
-// 友盟统计代码
-
-window._czc = window._czc || []
-window._czc.push(['_setAccount', '1279994399'])
-// 友盟统计
-// category 表示事件发生在谁身上，如视频，小说等
-// action 表示访客跟元素的交互行为，如播放、收藏
-// label 更详细的时间描述
-function uMengTj(category,action,label) {
-  window._czc.push(['_trackEvent',category,action,label])
-}
-window.uMengTj = uMengTj
-
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?1dc714d7635030ddfeae77dce303c820";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
